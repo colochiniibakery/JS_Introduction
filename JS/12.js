8a93b3d50e51de5cb812b7eb560e8f7fe9a6d130
colochinii <colochiniibakery@gmail.com>
2023-07-31 14:41:16 -0700

    actualizacion y adicion 13_js

--- JS/12.js
+++ JS/12.js
@@ -1,8 +1,9 @@
-const nombre ={
+// Describes the package 
+const cliente={
     nombre:"arturo",
     edad : 45,
     altrura: 180,
 
 }
-
+// function to debug in console
 console.log(nombre);
---
+++ JS/13_functions_introduction.js
@@ -0,0 +1,22 @@
+// declaration from function
+
+
+function sumar(){
+
+    console.log(10*10);
+}
+
+//here you called the function
+sumar();
+
+//expresion of the function
+
+const sumar2= function(){
+
+    console.log(3+3);
+
+}
+
+// called the function
+
+sumar2();
